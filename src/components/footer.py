a0d0ee170831ec7b834870816b1d6d70f60610bc
Saif Chogle <choglesaif5@gmail.com>
2026-07-01 15:41:11 +0530

    Initial release: SnapClass AI

---
+++ src/components/footer.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+def footer_home():
+    st.markdown("""
+        <div style="
+            margin-top:2rem;
+            text-align:center;
+            color:black;
+            font-size:16px;
+            font-weight:500;
+            letter-spacing:0.5px;
+        ">
+            © 2026 SnapClass AI • Developed by
+            <span style="
+                color:#FFD43B;
+                font-weight:700;
+            ">
+                Saif Chogle
+            </span>
+        </div>
+    """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    st.markdown("""
+        <div style="
+            margin-top:2.5rem;
+            text-align:center;
+            color:#444;
+            font-size:16px;
+            font-weight:600;
+            letter-spacing:0.5px;
+        ">
+            © 2026 SnapClass AI • Built by
+            <span style="
+                color:#4F5DFF;
+                font-weight:700;
+            ">
+                Saif Chogle
+            </span>
+        </div>
+    """, unsafe_allow_html=True)
